900e9f0b8fd8844e2c77edbe467b8e022068ee16
TarasYanUA <wildmonach@gmail.com>
2023-09-14 18:04:41 +0300

    Test-case for Product page is almost ready!

--- src/test/java/GeneralSettings_Var1_Default.java
+++ src/test/java/GeneralSettings_Var1_Default.java
@@ -158,7 +158,7 @@ public class GeneralSettings_Var1_Default extends TestRunner {
         makePause();
         screenshot("110 GeneralSettings_Var1_Default - Promotion page");
 
-        //Переходим на страницу товара с промо-акцией и проверяем все шаблоны страницы товара
+        //Переходим на страницу товара с промо-акцией
         stPromotions.chooseAnyProduct.click();
         //Проверяем, что шапка промо-акции присутствует на странице товара
         softAssert.assertTrue(stPromotions.promotionHeader.exists(),
@@ -170,47 +170,6 @@ public class GeneralSettings_Var1_Default extends TestRunner {
         screenshot("112 GeneralSettings_Var1_Default - Product page, Default");
         selectLanguage_RTL();
         screenshot("114 GeneralSettings_Var1_Default - Product page, Default (RTL)");
-        String productCode = $("span[id^='product_code_']").getText(); //Берём код товара
-        shiftBrowserTab(0);
-        csCartSettings.field_SearchOnTop.click();
-        csCartSettings.field_SearchOnTop.setValue(productCode).sendKeys(Keys.ENTER);
-        csCartSettings.productTemplate.selectOptionByValue("bigpicture_template");
-        goToProductPage(2);
-        screenshot("116 GeneralSettings_Var1_Default - Product page, BigPicture");
-        selectLanguage_RTL();
-        screenshot("118 GeneralSettings_Var1_Default - Product page, BigPicture (RTL)");
-        selectProductTemplate("abt__ut2_bigpicture_flat_template");
-        goToProductPage(3);
-        screenshot("120 GeneralSettings_Var1_Default - Product page, BigPictureFlat");
-        selectLanguage_RTL();
-        screenshot("122 GeneralSettings_Var1_Default - Product page, BigPictureFlat (RTL)");
-        selectProductTemplate("abt__ut2_bigpicture_gallery_template");
-        goToProductPage(4);
-        $(".ab__deal_of_the_day").scrollIntoView("{behavior: \"instant\", block: \"center\", inline: \"center\"}");
-        screenshot("124 GeneralSettings_Var1_Default - Product page, Gallery");
-        selectLanguage_RTL();
-        $(".ab__deal_of_the_day").scrollIntoView("{behavior: \"instant\", block: \"center\", inline: \"center\"}");
-        screenshot("126 GeneralSettings_Var1_Default - Product page, Gallery (RTL)");
-        selectProductTemplate("abt__ut2_three_columns_template");
-        goToProductPage(5);
-        screenshot("128 GeneralSettings_Var1_Default - Product page, Three-columned");
-        selectLanguage_RTL();
-        screenshot("130 GeneralSettings_Var1_Default - Product page, Three-columned (RTL)");
         softAssert.assertAll();
-    }
-
-    public void selectProductTemplate(String templateValue) {
-        shiftBrowserTab(0);
-        CsCartSettings csCartSettings = new CsCartSettings();
-        csCartSettings.productTemplate.selectOptionByValue(templateValue);
-    }
-
-    public void goToProductPage(int tabNumber){
-        CsCartSettings csCartSettings = new CsCartSettings();
-        csCartSettings.button_Save.click();
-        csCartSettings.gearWheelOnTop.click();
-        csCartSettings.button_Preview.click();
-        getWebDriver().getWindowHandle(); switchTo().window(tabNumber);
-        makePause();
     }
 }
--- src/test/java/promotionSettings/ProductPage.java
+++ src/test/java/promotionSettings/ProductPage.java
@@ -4,6 +4,11 @@ import generalSettings.TestRunner;
 import org.openqa.selenium.Keys;
 import org.testng.annotations.Test;
 import adminPanel.CsCartSettings;
+import org.testng.asserts.SoftAssert;
+import storefront.StPromotions;
+
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
 
 /*
 Настройки на странице промо-акции:
@@ -19,10 +24,63 @@ public class ProductPage extends TestRunner {
         CsCartSettings csCartSettings = new CsCartSettings();
         csCartSettings.field_SearchOnTop.click();
         csCartSettings.field_SearchOnTop.setValue("M0219A3GX3").sendKeys(Keys.ENTER);
+        csCartSettings.productTemplate.selectOptionByValue("default_template");
         csCartSettings.gearWheelOnTop.click();
         csCartSettings.button_Preview.click();
         shiftBrowserTab(1);
         selectLanguage_RU();
+        StPromotions stPromotions = new StPromotions();
+        SoftAssert softAssert = new SoftAssert();
+        //Проверяем, что шапка промо-акции присутствует на странице товара
+        softAssert.assertTrue(stPromotions.promotionHeader.exists(),
+                "There is no promotion header on the product page!");
+        //Проверяем, что присутствует FlipClock счётчик на страницу товара
+        softAssert.assertTrue(stPromotions.flipClock.exists(),
+                "Countdown type is not FlipClock on the product page!");
+        makePause();
+        screenshot("112 GeneralSettings_Var1_Default - Product page, Default");
+        selectLanguage_RTL();
+        screenshot("114 GeneralSettings_Var1_Default - Product page, Default (RTL)");
+        String productCode = $("span[id^='product_code_']").getText(); //Берём код товара
+        shiftBrowserTab(0);
+        csCartSettings.field_SearchOnTop.click();
+        csCartSettings.field_SearchOnTop.setValue(productCode).sendKeys(Keys.ENTER);
+        csCartSettings.productTemplate.selectOptionByValue("bigpicture_template");
+        goToProductPage(2);
+        screenshot("116 GeneralSettings_Var1_Default - Product page, BigPicture");
+        selectLanguage_RTL();
+        screenshot("118 GeneralSettings_Var1_Default - Product page, BigPicture (RTL)");
+        selectProductTemplate("abt__ut2_bigpicture_flat_template");
+        goToProductPage(3);
+        screenshot("120 GeneralSettings_Var1_Default - Product page, BigPictureFlat");
+        selectLanguage_RTL();
+        screenshot("122 GeneralSettings_Var1_Default - Product page, BigPictureFlat (RTL)");
+        selectProductTemplate("abt__ut2_bigpicture_gallery_template");
+        goToProductPage(4);
+        $(".ab__deal_of_the_day").scrollIntoView("{behavior: \"instant\", block: \"center\", inline: \"center\"}");
+        screenshot("124 GeneralSettings_Var1_Default - Product page, Gallery");
+        selectLanguage_RTL();
+        $(".ab__deal_of_the_day").scrollIntoView("{behavior: \"instant\", block: \"center\", inline: \"center\"}");
+        screenshot("126 GeneralSettings_Var1_Default - Product page, Gallery (RTL)");
+        selectProductTemplate("abt__ut2_three_columns_template");
+        goToProductPage(5);
+        screenshot("128 GeneralSettings_Var1_Default - Product page, Three-columned");
+        selectLanguage_RTL();
+        screenshot("130 GeneralSettings_Var1_Default - Product page, Three-columned (RTL)");
+        softAssert.assertAll();
     }
 
+    public void selectProductTemplate(String templateValue) {
+        shiftBrowserTab(0);
+        CsCartSettings csCartSettings = new CsCartSettings();
+        csCartSettings.productTemplate.selectOptionByValue(templateValue);
+    }
+    public void goToProductPage(int tabNumber){
+        CsCartSettings csCartSettings = new CsCartSettings();
+        csCartSettings.button_Save.click();
+        csCartSettings.gearWheelOnTop.click();
+        csCartSettings.button_Preview.click();
+        getWebDriver().getWindowHandle(); switchTo().window(tabNumber);
+        makePause();
+    }
 }
